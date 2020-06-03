947ccd4e544be33a68aca2e70f70a8815d6aee0c
Kayla Harwell <kayla.h.7556@gmail.com>
2020-06-03 16:31:38 +0000

    Chnage var names to lowerCamelCase

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -18,19 +18,19 @@
 
 
 function KaylaTrivia(){
-   const correct_answers = ['b','a','b','c'] ;
+   const correctAnswers = ['b','a','b','c'] ;
    const questions= ['Which hackathon did my team take home our first win? \n a-Google b-Amazon Games c-Bison Hacks','Where am I from? \n a-Buffalo b-NYC c-LA','Out of these places, which one do I want to travel to the most? \n a-Utah b-Italy c-France','In which one of these orgs do I hold a E-Board position? \n a-Student Council b-NSBE c-ACM'];
    var points = 0;
-   var user_ans;
+   var userAns;
     
     for(i=0;i< questions.length;i++){
         user_ans = prompt(questions[i], "???");
-        if(user_ans==correct_answers[i]){
+        if(userAns==correctAnswers[i]){
             points+=1;
-            alert('Correct! You have a score of '+points+' points');
+            alert(`Correct! You have a score of ${points} points`);
         }
         else{
-             alert('Incorrect:( You have a score of '+points+' points');
+             alert(`Incorrect:( You have a score of ${points} points`);
         }
     }
 
