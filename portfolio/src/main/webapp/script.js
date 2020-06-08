d8edc08035a640eea73368d9b64989259f95bf26
Kayla Harwell <kayla.h.7556@gmail.com>
2020-06-08 20:45:04 +0000

    make format

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,30 +16,32 @@
  * Adds a random greeting to the page.
  */
 
+function KaylaTrivia() {
+  const correctAnswers = ['b', 'a', 'b', 'c'];
+  const questions = [
+    'Which hackathon did my team take home our first win? \n a-Google b-Amazon Games c-Bison Hacks',
+    'Where am I from? \n a-Buffalo b-NYC c-LA',
+    'Out of these places, which one do I want to travel to the most? \n a-Utah b-Italy c-France',
+    'In which one of these orgs do I hold a E-Board position? \n a-Student Council b-NSBE c-ACM',
+  ];
+  var points = 0;
+  var userAns;
 
-function KaylaTrivia(){
-   const correctAnswers = ['b','a','b','c'] ;
-   const questions= ['Which hackathon did my team take home our first win? \n a-Google b-Amazon Games c-Bison Hacks','Where am I from? \n a-Buffalo b-NYC c-LA','Out of these places, which one do I want to travel to the most? \n a-Utah b-Italy c-France','In which one of these orgs do I hold a E-Board position? \n a-Student Council b-NSBE c-ACM'];
-   var points = 0;
-   var userAns;
-    
-    for(i=0;i< questions.length;i++){
-        user_ans = prompt(questions[i], "???");
-        if(userAns==correctAnswers[i]){
-            points+=1;
-            alert(`Correct! You have a score of ${points} points`);
-        }
-        else{
-             alert(`Incorrect:( You have a score of ${points} points`);
-        }
+  for (i = 0; i < questions.length; i++) {
+    user_ans = prompt(questions[i], '???');
+    if (userAns == correctAnswers[i]) {
+      points += 1;
+      alert(`Correct! You have a score of ${points} points`);
+    } else {
+      alert(`Incorrect:( You have a score of ${points} points`);
     }
+  }
 
-    if(points >=3){
-        alert(`Your final score is ${points}  Yay you did great!`);
-    }
-    else{
-         alert(`Your final score is ${points}  Oof maybe try again?`);
-    }
+  if (points >= 3) {
+    alert(`Your final score is ${points}  Yay you did great!`);
+  } else {
+    alert(`Your final score is ${points}  Oof maybe try again?`);
+  }
 }
 function getMessage() {
   /** The fetch() function returns a Promise because the request is asynchronous. */
@@ -50,28 +52,22 @@ function getMessage() {
 }
 
 function handleResponse(response) {
-
-
   /** response.text() returns a Promise, because the response is a stream of
-  * content and not a simple variable.
-  */
+   * content and not a simple variable.
+   */
   const textPromise = response.text();
 
   /**When the response is converted to text, pass the result into the
-  * addQuoteToDom() function.
-  */
+   * addQuoteToDom() function.
+   */
   textPromise.then(addQuoteToDom);
 }
 
-/** Passing quote to function so that it can be given to the DOM 
-* @param {string} quote string 
-*/
+/** Passing quote to function so that it can be given to the DOM
+ * @param {string} quote string
+ */
 
 function addQuoteToDom(quote) {
   const quoteContainer = document.getElementById('quote-container');
   quoteContainer.innerText = quote;
 }
-
-
-
-
