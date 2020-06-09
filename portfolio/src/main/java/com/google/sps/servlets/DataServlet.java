c56bed6511a23cef6936998c55d3d76e81cccc38
Kayla Harwell <kayla.h.7556@gmail.com>
2020-06-09 22:54:33 +0000

    add comment section

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -25,33 +25,33 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
   ArrayList<String> greetings = new ArrayList<String>();
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Convert greetings into a json string
     String json = convertToJson(greetings);
     // Send the JSON as the response
     response.setContentType("application/json;");
-    
+
     response.getWriter().println(json);
   }
 
   private String convertToJson(ArrayList<String> greetings) {
-    String json = "hi";
+    String json = "";
     for (int i = 0; i < greetings.size(); i++) {
       json += "\n" + greetings.get(i);
     }
     return json;
-
   }
 
-   @Override
+  @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Get the input from the form.
     String text = request.getParameter("user-ans");
-   greetings.add(text);
+    greetings.add(text);
 
     // Respond with the result.
     response.setContentType("text/html;");
-  response.sendRedirect("/index.html");
+    response.sendRedirect("/index.html");
   }
 }
