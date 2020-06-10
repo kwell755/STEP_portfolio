fd5da705bb8233d7d366dd76e17f0fc2e2987a7d
Kayla Harwell <kayla.h.7556@gmail.com>
2020-06-10 17:30:34 +0000

    add clarity to code by changing var names and deleteing unused objects

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -29,30 +29,29 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that returns some example content. TODO: modify this file to handle comments data */
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-  ArrayList<String> greetings = new ArrayList<String>();
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // Convert greetings into a json string
-    String json = convertToJson(greetings);
+   
 
-    ArrayList<String> tasks = new ArrayList<>();
+   
     Query query = new Query("Comment");
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = datastore.prepare(query);
+     ArrayList<String> tasks = new ArrayList<>();
     for (Entity entity : results.asIterable()) {
-      String commentEx = (String) entity.getProperty("message");
-      tasks.add(commentEx);
+      String savedMessage = (String) entity.getProperty("message");
+      tasks.add(savedMessage);
     }
 
     response.setContentType("application/json;");
     response.getWriter().println(convertToJson(tasks));
   }
 
-  private String convertToJson(ArrayList<String> greetings) {
+  private String convertToJson(ArrayList<String> message) {
     String json = "";
-    for (int i = 0; i < greetings.size(); i++) {
-      json += "\n" + greetings.get(i);
+    for (int i = 0; i < message.size(); i++) {
+      json += "\n" + message.get(i);
     }
     return json;
   }
@@ -61,7 +60,7 @@ public class DataServlet extends HttpServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Get the input from the form.
     String text = request.getParameter("user-ans");
-    greetings.add(text);
+  
 
     // Respond with the result.
     response.setContentType("text/html;");
