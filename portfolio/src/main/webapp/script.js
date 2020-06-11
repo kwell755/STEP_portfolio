b13df574b48bbe3eb6a5230425f7d40b9e18b561
Kayla Harwell <kayla.h.7556@gmail.com>
2020-06-11 23:16:55 +0000

    add google maps library to portfolio website

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -44,7 +44,7 @@ function kaylaTrivia() {
   }
 }
 
-window.getMessage = getMessage;
+
 window.onload = getMessage;
 function getMessage() {
   // eslint-disable-line
@@ -74,4 +74,10 @@ function handleResponse(response) {
 function addQuoteToDom(quote) {
   const quoteContainer = document.getElementById('quote-container');
   quoteContainer.innerText = quote;
+}
+
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
 }
